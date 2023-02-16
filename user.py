fc389aefcd778bb80365154d1b933844249df462
Muhammad Nouman <nomi9639@yahoo.com>
2023-02-16 12:10:48 +0300

    Task 1 for Enabled User, Task 2 Update First and Last name as per Email

--- user.py
+++ user.py
@@ -8,9 +8,22 @@ class User(Document):
 
 @frappe.whitelist(allow_guest=True, xss_safe=False, methods=["GET"])
 def get_users_list():
-    print "Your logic!"
-
+    users = frappe.get_all("User", {"enabled": 1})
+    return frappe.as_json(users)
 
 @frappe.whitelist(allow_guest=True, xss_safe=False, methods=["POST"])
 def update_user_name(email, first_name, last_name):
-    print "Your logic!"
+    if frappe.db.exists("User", {"email": email}):
+        user = frappe.get_doc("User", {"email": email})
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        response = {
+            "id": user.name,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name
+        }
+        return frappe.as_json(response)
+    else:
+        return frappe.as_json({"error": "User not found"})
